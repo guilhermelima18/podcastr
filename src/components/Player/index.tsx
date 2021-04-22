53a083d04524100c8f0df9fcec41ef8042aed0ca
guilhermelima18 <guilhermelima18@hotmail.com>
2021-04-22 16:08:07 -0300

    dia 4

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -17,49 +17,49 @@ export default function Player() {
 
       { episode ? (
         <div className={styles.currentEpisode}>
-          <Image 
-            width={592} 
-            height={592} 
+          <Image
+            width={592}
+            height={592}
             src={episode.thumbnail}
             objectFit="cover"
           />
           <strong>{episode.title}</strong>
           <strong>{episode.members}</strong>
         </div>
-        ) : (
-            <div className = {styles.emptyPlayer}>
-              <strong>Selecione um Podcast para ouvir</strong>
-            </div>
-          )
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um Podcast para ouvir</strong>
+        </div>
+      )
       }
 
-<footer className={styles.empty}>
-  <div className={styles.progress}>
-    <span>00:00</span>
-    <div className={styles.slider}>
-      <div className={styles.emptySlider} />
-    </div>
-    <span>00:00</span>
-  </div>
+      <footer className={styles.empty}>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+            <div className={styles.emptySlider} />
+          </div>
+          <span>00:00</span>
+        </div>
 
-  <div className={styles.buttons}>
-    <button type="button">
-      <img src="/shuffle.svg" alt="Embaralhar" />
-    </button>
-    <button type="button">
-      <img src="/play-previous.svg" alt="Tocar anterior" />
-    </button>
-    <button type="button" className={styles.playButton}>
-      <img src="/play.svg" alt="Tocar" />
-    </button>
-    <button type="button">
-      <img src="/play-next.svg" alt="Tocar próxima" />
-    </button>
-    <button type="button">
-      <img src="/repeat.svg" alt="Repetir" />
-    </button>
-  </div>
-</footer>
+        <div className={styles.buttons}>
+          <button type="button">
+            <img src="/shuffle.svg" alt="Embaralhar" />
+          </button>
+          <button type="button">
+            <img src="/play-previous.svg" alt="Tocar anterior" />
+          </button>
+          <button type="button" className={styles.playButton}>
+            <img src="/play.svg" alt="Tocar" />
+          </button>
+          <button type="button">
+            <img src="/play-next.svg" alt="Tocar próxima" />
+          </button>
+          <button type="button">
+            <img src="/repeat.svg" alt="Repetir" />
+          </button>
+        </div>
+      </footer>
     </div >
   );
 }
