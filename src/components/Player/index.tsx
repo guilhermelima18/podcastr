f6b45b5fa88e5f7fae3ecd59082cce7c987916cd
guilhermelima18 <guilhermelima18@hotmail.com>
2021-04-22 16:06:35 -0300

    dia 4

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,13 @@
+import Image from 'next/image';
+import { useContext } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
 
 export default function Player() {
+  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
+
+  const episode = episodeList[currentEpisodeIndex];
+
   return (
     <div className={styles.playerContainer}>
       <header>
@@ -8,37 +15,51 @@ export default function Player() {
         <strong>Tocando agora</strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um Podcast para ouvir</strong>
-      </div>
-
-      <footer className={styles.empty}>
-        <div className={styles.progress}>
-          <span>00:00</span>
-          <div className={styles.slider}>
-            <div className={styles.emptySlider} />
-          </div>
-          <span>00:00</span>
+      { episode ? (
+        <div className={styles.currentEpisode}>
+          <Image 
+            width={592} 
+            height={592} 
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <strong>{episode.members}</strong>
         </div>
+        ) : (
+            <div className = {styles.emptyPlayer}>
+              <strong>Selecione um Podcast para ouvir</strong>
+            </div>
+          )
+      }
 
-        <div className={styles.buttons}>
-          <button type="button">
-            <img src="/shuffle.svg" alt="Embaralhar" />
-          </button>
-          <button type="button">
-            <img src="/play-previous.svg" alt="Tocar anterior" />
-          </button>
-          <button type="button" className={styles.playButton}>
-            <img src="/play.svg" alt="Tocar" />
-          </button>
-          <button type="button">
-            <img src="/play-next.svg" alt="Tocar próxima" />
-          </button>
-          <button type="button">
-            <img src="/repeat.svg" alt="Repetir" />
-          </button>
-        </div>
-      </footer>
+<footer className={styles.empty}>
+  <div className={styles.progress}>
+    <span>00:00</span>
+    <div className={styles.slider}>
+      <div className={styles.emptySlider} />
     </div>
+    <span>00:00</span>
+  </div>
+
+  <div className={styles.buttons}>
+    <button type="button">
+      <img src="/shuffle.svg" alt="Embaralhar" />
+    </button>
+    <button type="button">
+      <img src="/play-previous.svg" alt="Tocar anterior" />
+    </button>
+    <button type="button" className={styles.playButton}>
+      <img src="/play.svg" alt="Tocar" />
+    </button>
+    <button type="button">
+      <img src="/play-next.svg" alt="Tocar próxima" />
+    </button>
+    <button type="button">
+      <img src="/repeat.svg" alt="Repetir" />
+    </button>
+  </div>
+</footer>
+    </div >
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,32 @@ import Player from '../components/Player';
 
 import styles from '../styles/app.module.scss';
 import Head from 'next/head';
+import { PlayerContext } from '../contexts/PlayerContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  };
+
   return (
     <>
       <Head>
         <title>PodcastR - Sua plataforma de Podcasts</title>
       </Head>
-      <div className={styles.appContainer}>
-        <main>
-          <Header />
-          <Component {...pageProps} />
-        </main>
-        <Player />
-      </div>
+      <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+        <div className={styles.appContainer}>
+          <main>
+            <Header />
+            <Component {...pageProps} />
+          </main>
+          <Player />
+        </div>
+      </PlayerContext.Provider>
     </>
   );
 }
